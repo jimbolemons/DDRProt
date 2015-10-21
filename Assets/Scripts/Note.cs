d0d13d8fa53fe0d5c08c099b3f4939fdec790911
jimbolemons <drummond.jacob13@gmail.com>
2015-10-21 13:27:07 -0400

    First_Comit

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+	public float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 pos = transform.position;
+		pos.y = 6.2f;
+		transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.y -= speed * Time.deltaTime;
+		transform.position = pos;
+
+		//////////// If off the bottom of the screen, destroy this object:
+		if (pos.y < -8) Destroy (gameObject);
+
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (Input.GetKeyDown(KeyCode.A)) {
+			if (col.gameObject.tag == "Yellow") 
+			{
+				Destroy (gameObject);
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			if (col.gameObject.tag == "Red")
+			{
+				Destroy (gameObject);				
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			if (col.gameObject.tag == "Blue")
+			{
+				Destroy (gameObject);				
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.F)) {
+			if (col.gameObject.tag == "Green")
+			{
+				Destroy (gameObject);				
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+	
+	/// <summary>
+	/// The enemy prefab to spawn.
+	/// </summary>
+	public GameObject notePrefab1;
+	public GameObject notePrefab2;
+	public GameObject notePrefab3;
+	public GameObject notePrefab4;
+	
+	void Start () {
+		StartCoroutine ("StartSpawning1");
+		StartCoroutine ("StartSpawning2");
+		StartCoroutine ("StartSpawning3");
+		StartCoroutine ("StartSpawning4");
+	}
+	/// <summary>
+	/// 
+	/// Spawns an enemy.
+	/// </summary>
+	GameObject SpawnNote1(){
+	
+		return Instantiate (notePrefab1);
+
+	}
+	GameObject SpawnNote2(){
+
+		return Instantiate (notePrefab2);
+		
+	}
+	GameObject SpawnNote3(){
+
+		return Instantiate (notePrefab3);
+		
+	}
+	GameObject SpawnNote4(){
+
+		return Instantiate (notePrefab4);
+		
+	}
+	/// <summary>
+	/// A coroutine that spawns enemies every half second.
+	/// </summary>
+	IEnumerator StartSpawning1(){
+		while(true){
+
+			SpawnNote1();
+			yield return new WaitForSeconds(Random.Range(.75f,5f));
+		}
+
+	}
+	IEnumerator StartSpawning2(){
+		while(true){
+			
+			SpawnNote2();
+			yield return new WaitForSeconds(Random.Range(.75f,5f));
+		}
+		
+	}
+	IEnumerator StartSpawning3(){
+		while(true){
+			
+			SpawnNote3();
+			yield return new WaitForSeconds(Random.Range(.75f,5f));
+		}
+		
+	}
+	IEnumerator StartSpawning4(){
+		while(true){
+			
+			SpawnNote4();
+			yield return new WaitForSeconds(Random.Range(.75f,5f));
+		}
+		
+	}
+	
+}
